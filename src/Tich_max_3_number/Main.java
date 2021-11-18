b760b1ed7c437b7d988a7a93e76d8bef46c1e845
Lap <lapconghoang@gmail.com>
2021-11-18 14:03:20 +0700

    update

--- src/Tich_max_3_number/Main.java
+++ src/Tich_max_3_number/Main.java
@@ -8,6 +8,9 @@ import java.util.Scanner;
  */
 public class Main {
 
+    
+    
+    
     public static void main(String[] args) {
         int n;
         Scanner scanner = new Scanner(System.in);
